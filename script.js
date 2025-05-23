ceb28df807fdcc4c1bdb560e65caed5597056d6d
sharmababita <sharma.089babita@gmail.com>
2025-05-23 11:02:23 +0200

    add the value

--- script.js
+++ script.js
@@ -11,14 +11,23 @@ const displayResults = document.querySelector('#results');
 const displayWins = document.querySelector("#wins");
 const displayDraws = document.querySelector("#draws");
 const displayLosses = document.querySelector("#losses");
+const displayCash = document.querySelector("#displayCash");
+const displayCost = document.querySelector("#displayCost");
 
 let wins = 0;
 let draws = 0;
 let losses = 0;
-
+let cash = 1000;
+let cost = 100 ;
 let yourChoice;
 
 const choices = ['rock', 'paper', 'scissors'];
+
+displayCash.innerHTML =  "Cash: " + cash;
+displayCost.innerHTML =  "Cost: " + cost;
+displayWins.innerHTML =  "Draws: " + draws;
+displayWins.innerHTML =  "wins: " + wins;
+displayWins.innerHTML =  "Losses: " + losses;
 
 function selectRock() {
   
@@ -46,6 +55,11 @@ function selectScissors() {
 
 scissorsButton.addEventListener('click', selectScissors);
 
+displayCash.innerHTML ="Cash: " + cash;
+displayWins.innerHTML ="Wins: " + wins;
+displayDraws.innerHTML ="Draws: " + draws;
+displayLosses.innerHTML ="Losses: " + losses;
+
 function computerChoice() {
   
   let computerNumber = choices[Math.floor(Math.random() * 3)];
@@ -57,26 +71,26 @@ function computerChoice() {
   if (yourChoice === computerNumber) {
     displayResults.innerHTML = 'Its a draw!';
     draws++
-displayDraws.innerHTML = draws;
+displayDraws.innerHTML ="Draws: " + draws;
     
   } else if (computerNumber === 'rock' && yourChoice === 'paper') {
     displayResults.innerHTML = 'You win!';
     wins++
-    displayWins.innerHTML = wins;
+    displayWins.innerHTML =  "wins: " + wins;
   } else if (computerNumber === 'paper' && yourChoice === 'scissors') {
     displayResults.innerHTML = 'You win!';
     wins++
-    displayWins.innerHTML = wins;
+    displayWins.innerHTML =  "wins: " + wins;
 
   } else if (computerNumber === 'scissors' && yourChoice === 'rock') {
     displayResults.innerHTML = 'You win!';
    wins++
- displayWins.innerHTML = wins;
+   displayWins.innerHTML =  "wins: " + wins;
 
   } else {
     displayResults.innerHTML = 'You lose!';
     losses++
-    displayLosses.innerHTML = losses;
+    displayLosses.innerHTML = "losses: " + losses;
 
   }
 }
